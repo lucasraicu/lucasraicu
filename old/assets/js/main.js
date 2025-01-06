97412f5e11ffeda45712c9b103ca37dc9b1b8a3d
johnny-raicu <johnny.raicu@gmail.com>
2025-01-05 22:14:05 -0600

    first commit of new site

--- old/assets/js/main.js
+++ old/assets/js/main.js
@@ -8,7 +8,7 @@
 
 	// Breakpoints.
 		skel.breakpoints({
-			xlarge:	'(max-width: 1920px)',
+			xlarge:	'(max-width: 1680px)',
 			large:	'(max-width: 1280px)',
 			medium:	'(max-width: 980px)',
 			small:	'(max-width: 736px)',
